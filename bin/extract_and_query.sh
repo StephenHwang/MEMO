c5ea80c2ce93abebefef0bf7fa86d35e874721e7
StephenHwang <sjhwang@ucsc.edu>
2023-10-04 16:26:31 -0400

    Add python and bash query scripts.

---
+++ bin/extract_and_query.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# basic params
+K=12
+BED_FILE=
+NUM_ORDER_MEMS=
+QUERY_REGION=
+OUTPUT_DIR='.'
+SORT_OUTPUT_BED='false'
+
+usage() {
+echo \
+"
+omem extract and query - extract region chr:start-end from overlap order MEM index and cast k-shadows
+Usage: ./extract_and_query.sh [options]
+
+Options:
+  -b [FILE]              compressed, indexed bed file of overlap order MEMs
+  -r [CHR:START-END]     target query region to extract from indexed omem bed
+  -k [INT]               k-mer size [12]
+  -n [INT]               number of other (non-pivot) documents in pangenome
+  -o [FILE]              output directory ['/.']
+  -s                     sort bed output
+"
+exit 0
+}
+
+if [ "$#" -eq 0 ] || [ "$1" = "-h" ]; then
+    usage
+fi
+
+# parse flags
+while getopts "b:r:k:n:o:s" OPTION
+do
+    case $OPTION in
+        b )
+            BED_FILE=$OPTARG
+            ;;
+        r )
+            QUERY_REGION=$OPTARG
+            ;;
+        k )
+            K=$OPTARG
+            ;;
+        n )
+            NUM_ORDER_MEMS=$OPTARG
+            ;;
+        o )
+            OUTPUT_DIR=$OPTARG
+            ;;
+        s )
+            SORT_OUTPUT_BED='true'
+            ;;
+        * )
+            usage
+            ;;
+    esac
+done
+
+################################################################################
+# subtract the size of the k-mer from the end and re-assign windows
+echo -e "\nCasting shadows"
+cast_awk_shadows() {
+  awk -v k=$1 '{
+    d=$3-(k-1);
+    if (d < $2) {
+      $3=$2;
+      $2=d*(d>0)
+    }
+    else {
+      next
+    }
+    }1' OFS='\t'
+}
+export -f cast_awk_shadows
+################################################################################
+
+# Parse QUERY_REGION into record, start, end
+QUERY_CHR=$(echo $QUERY_REGION | cut -d':' -f1)
+QUERY_START_END=$(echo $QUERY_REGION | cut -d':' -f2)
+QUERY_START=$(echo $QUERY_START_END | cut -d'-' -f1)
+QUERY_END=$(echo $QUERY_START_END | cut -d'-' -f2)
+
+# Start query
+echo -e "\nQuerying $QUERY_CHR : $QUERY_START - $QUERY_END"
+OUT_FILE=$OUTPUT_DIR/omem_olaps_${QUERY_CHR}_${QUERY_START}_${QUERY_END}.bed
+
+# Make query bed of desired regions
+echo -e "$QUERY_CHR\t$QUERY_START\t$QUERY_END" > $OUTPUT_DIR/query.bed
+
+# extract window and cast shadows
+tabix $BED_FILE $QUERY_CHR:$QUERY_START-$QUERY_END | \
+  bedtools intersect -sorted -wa -f 1 -a "stdin" -b $OUTPUT_DIR/query.bed |
+  cast_awk_shadows $K \
+  > $OUT_FILE.tmp
+
+# merge intervals for each order
+echo "Merging windows"
+> $OUT_FILE     # rm file before re-populating
+parallel -j $NUM_ORDER_MEMS \
+  "grep -w {}$ $OUT_FILE.tmp |
+  bedtools merge -c 4 -o first" \
+  ::: $(seq 1 $NUM_ORDER_MEMS) \
+  >> $OUT_FILE
+rm $OUT_FILE.tmp
+
+# Sort the final bed file
+if [ "$SORT_OUTPUT_BED" = true ] ; then
+  echo "Sorting final bed"
+  sort -k1,1V -k2,2n -o $OUT_FILE $OUT_FILE
+fi
+
+echo "Output bed file: $OUT_FILE"
--- bin/pyarrow_query.py
+++ bin/pyarrow_query.py
@@ -2,34 +2,12 @@
 
 import numpy as np
 import pyarrow.dataset as ds
-
-in_file = "iterative_500m.parquet"    # "omem_bacteria.parquet"
-out_file = "shadow_casted_2.bed"
-
-query_record = "NZ_CP015023.1"
-query_start = 0
-query_end = 100   #5506800
-k = 12
-num_docs = 4
+import argparse
+import os
 
 
-# load parquet file into stream
-e_coli_mem_bed_pq_ds = ds.dataset(in_file, format="parquet")
-
-# extract and write to file
-x = np.array(
-  e_coli_mem_bed_pq_ds.to_table(
-    filter=(ds.field('f0') == query_record) &
-           (ds.field('f1') >= query_start) &
-           (ds.field('f2') <= query_end),
-      columns=['f1', 'f2', 'f3']
-    ).to_pandas(),
-    np.uint
-  )
-
-# filter and shadow cast
-k -= 1 # adjust k buffer
-def cast_shadows(row):
+def cast_shadows(row, k):
+    ''' Cast k-mer shadows. '''
     start, end, doc_idx = row
     diff = end - k
     if diff < start:
@@ -37,28 +15,74 @@ def cast_shadows(row):
         return start, end, doc_idx
     else:
         return [np.nan] * 3
-result = np.apply_along_axis(cast_shadows, axis=1, arr=x)
-result = result[~np.isnan(result).any(axis=1)].astype('uint')
 
-# merge intervals (of the same order)
-merged_intervals = []
-for doc_idx in range(1, num_docs+1):
-    # subset for recs of current doc
-    doc_idx_intervals = result[result[:,2] == doc_idx, 0:2]
-    index = 0
-    for i in range(1, len(doc_idx_intervals)):
-        if (doc_idx_intervals[index][1] >= doc_idx_intervals[i][0]):
-            doc_idx_intervals[index][1] = max(doc_idx_intervals[index][1],
-                                              doc_idx_intervals[i][1])
-        else:
-            index += 1
-            doc_idx_intervals[index] = doc_idx_intervals[i]
-    doc_idx_intervals = doc_idx_intervals[:index+1, ]
-    merged_intervals.append( np.insert(doc_idx_intervals, doc_idx_intervals.shape[1], doc_idx, axis=1) )
+def load_pq(in_file, query_record, query_start, query_end, k):
+    ''' Filter parquet bed file into k-mer shadow casted intervals. '''
+    e_coli_mem_bed_pq_ds = ds.dataset(in_file, format="parquet")
+    # extract and filter for desired region
+    genome_omems_arr =  np.array(
+      e_coli_mem_bed_pq_ds.to_table(
+        filter=(ds.field('f0') == query_record) &
+               (ds.field('f1') >= query_start) &
+               (ds.field('f2') <= query_end),
+          columns=['f1', 'f2', 'f3']
+        ).to_pandas(),
+        np.uint
+      )
+    result = np.apply_along_axis(cast_shadows, 1, genome_omems_arr, k)
+    result = result[~np.isnan(result).any(axis=1)].astype('uint')
+    return result
 
-# sort and print
-fully_merged_sorted_array = np.sort(np.concatenate(merged_intervals), axis=0)
+def merge_intervals(result, num_docs):
+    ''' Merge intervals. '''
+    merged_intervals = []
+    for doc_idx in range(1, num_docs+1):
+        # subset for recs of current doc
+        doc_idx_intervals = result[result[:,2] == doc_idx, 0:2]
+        index = 0
+        for i in range(1, len(doc_idx_intervals)):
+            if (doc_idx_intervals[index][1] >= doc_idx_intervals[i][0]):
+                doc_idx_intervals[index][1] = max(doc_idx_intervals[index][1],
+                                                  doc_idx_intervals[i][1])
+            else:
+                index += 1
+                doc_idx_intervals[index] = doc_idx_intervals[i]
+        doc_idx_intervals = doc_idx_intervals[:index+1, ]
+        merged_intervals.append( np.insert(doc_idx_intervals, doc_idx_intervals.shape[1], doc_idx, axis=1) )
+    # concat and sort
+    fully_merged_sorted_array = np.concatenate(merged_intervals)
+    return fully_merged_sorted_array[np.lexsort((fully_merged_sorted_array[:,1], fully_merged_sorted_array[:,0]))]
 
-print('Saving shadow casted bed to:', out_file)
-np.savetxt(out_file, fully_merged_sorted_array, delimiter="\t", fmt=query_record+'\t%1i\t%1i\t%1i')
+def save_to_file(arr, query_record, out_file):
+    np.savetxt(out_file, arr, delimiter="\t", fmt=query_record+'\t%1i\t%1i\t%1i')
 
+################################################################################
+
+def parse_arguments():
+    """ Parse and return the command-line arguments. """
+    parser = argparse.ArgumentParser(description="Extract and query overlap MEMs for k-mer presence/absence.")
+    parser.add_argument('-b', '--bed_file', dest='in_file', help='parquet bed file', required=True)
+    parser.add_argument('-o', '--out_file', dest='out_file', help='output bed file', required=True)
+    parser.add_argument('-n', '--ndocs', dest='num_docs', help='number of documents', required=True)
+    parser.add_argument('-k', '--kmer_size', dest='k', help='k-mer size', required=True)
+    parser.add_argument('-r', '--genome_region', dest='genome_region', help='genome region, formatted as record:start-end', required=True)
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    in_file = args.in_file
+    out_file = args.out_file
+    num_docs = int(args.num_docs)
+    k = int(args.k) - 1
+    genome_region = args.genome_region
+    query_record, start_end = genome_region.split(':')
+    query_start, query_end = map(int, start_end.split('-'))
+
+    res = load_pq(in_file, query_record, query_start, query_end, k)
+    mer_res = merge_intervals(res, num_docs)
+    save_to_file(mer_res, query_record, out_file)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
