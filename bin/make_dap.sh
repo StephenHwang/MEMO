e845f4795aa786db74ae9ca400e2402c2272fab2
StephenHwang <sjhwang@ucsc.edu>
2023-11-06 15:07:34 -0500

    Add scripts to go from .lengths files to DAP.

---
+++ bin/make_dap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# basic params
+LENGTHS_DIR=
+OUTDIR=
+
+usage() {
+echo \
+"
+Options:
+  -l [LENGTHS_DIR]       lengths directory
+  -o [OUTDIR]            output directory
+"
+exit 0
+}
+
+if [ "$#" -eq 0 ] || [ "$1" = "-h" ]; then
+    usage
+fi
+
+# parse flags
+while getopts "l:o:" OPTION
+do
+    case $OPTION in
+        l )
+            LENGTHS_DIR=$OPTARG
+            ;;
+        o )
+            OUTDIR=$OPTARG
+            ;;
+        * )
+            usage
+            ;;
+    esac
+done
+
+ls $LENGTHS_DIR | xargs -I file /scratch4/blangme2/sjhwang/omem/bin/verticalize_lengths.sh -f file -o $OUTDIR
+paste -d ' ' $(ls $OUTDIR/*.lengths | sort) | nl -v0 -w1 -s' ' > full_dap.txt
+
+
