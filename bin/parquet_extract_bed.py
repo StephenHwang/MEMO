258720f734312b57ea49efc8ea3c7857d83c3af8
StephenHwang <sjhwang@ucsc.edu>
2023-12-13 17:14:54 -0500

    Test different filter schemes.

--- bin/parquet_extract_bed.py
+++ bin/parquet_extract_bed.py
@@ -13,7 +13,8 @@ import argparse
 import os
 
 
-def filter_pq(in_file, query_record, query_start, query_end):
+# def filter_pq(in_file, query_record, query_start, query_end):
+def filter_pq_og(in_file, query_record, query_start, query_end):
     ''' Filter parquet bed file into k-mer shadow casted intervals. '''
     pq_ds = ds.dataset(in_file, format="parquet")
     qs_in_f_filter = ((ds.field('f0') == query_record) &
@@ -31,6 +32,38 @@ def filter_pq(in_file, query_record, query_start, query_end):
                             columns=['f1', 'f2', 'f3']
                            ).to_pandas(), np.uint)
     return np.concatenate([qs_in_f_arr, f1_in_qs_arr], axis=0)
+
+def filter_pq_simple(in_file, query_record, query_start, query_end):
+# def filter_pq(in_file, query_record, query_start, query_end):
+    ''' Filter parquet bed file into k-mer shadow casted intervals. '''
+    pq_ds = ds.dataset(in_file, format="parquet")
+    chr_filter = (ds.field('f0') == query_record)
+    simple_filter = (query_start <= (ds.field('f1'))) & ((ds.field('f1') < query_end))
+    joint_filter = chr_filter & simple_filter
+    return np.array(pq_ds.to_table(
+             filter=joint_filter,
+             columns=['f1', 'f2', 'f3']
+            ).to_pandas(), np.uint)
+
+
+# def filter_pq_(in_file, query_record, query_start, query_end):
+def filter_pq(in_file, query_record, query_start, query_end):
+    ''' Filter parquet bed file into k-mer shadow casted intervals. '''
+    pq_ds = ds.dataset(in_file, format="parquet")
+    chr_filter = (ds.field('f0') == query_record)
+    qs_in_f_filter = ((ds.field('f1') <= query_start) & (ds.field('f2') >  query_start))
+    f1_in_qs_filter = ((ds.field('f1') >  query_start) & (ds.field('f1') <  query_end))
+    joint_filter = chr_filter & (qs_in_f_filter | f1_in_qs_filter)
+    return pq_ds.to_table(
+             filter=joint_filter,
+             columns=['f1', 'f2', 'f3'],
+             use_threads=True
+            )
+    # return np.array(pq_ds.to_table(
+             # filter=joint_filter,
+             # columns=['f1', 'f2', 'f3'],
+             # use_threads=True
+            # ).to_pandas(), np.uint)
 
 
 def save_to_file(arr, query_record, out_file):
@@ -55,7 +88,7 @@ def main(args):
     query_record, start_end = genome_region.split(':')
     query_start, query_end = map(int, start_end.split('-'))
     res = filter_pq(in_file, query_record, query_start, query_end)
-    save_to_file(res, query_record, out_file)
+    # save_to_file(res, query_record, out_file)
 
 
 if __name__ == "__main__":
