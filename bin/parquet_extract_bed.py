3243d611c40f282a2175d794b4bbb12d89b4cb05
StephenHwang <sjhwang@ucsc.edu>
2023-12-21 22:37:21 -0500

    In progress save pq to file

--- bin/parquet_extract_bed.py
+++ bin/parquet_extract_bed.py
@@ -9,44 +9,11 @@
 import numpy as np
 import pandas as pd
 import pyarrow.dataset as ds
+from pyarrow import csv
 import argparse
 import os
 
 
-# def filter_pq(in_file, query_record, query_start, query_end):
-def filter_pq_og(in_file, query_record, query_start, query_end):
-    ''' Filter parquet bed file into k-mer shadow casted intervals. '''
-    pq_ds = ds.dataset(in_file, format="parquet")
-    qs_in_f_filter = ((ds.field('f0') == query_record) &
-                      (ds.field('f1') <= query_start) &
-                      (ds.field('f2') >  query_start))
-    f1_in_qs_filter = ((ds.field('f0') == query_record) &
-                       (ds.field('f1') >  query_start) &
-                       (ds.field('f1') <  query_end))
-    qs_in_f_arr = np.array(pq_ds.to_table(
-                           filter=qs_in_f_filter,
-                           columns=['f1', 'f2', 'f3']
-                          ).to_pandas(), np.uint)
-    f1_in_qs_arr = np.array(pq_ds.to_table(
-                            filter=f1_in_qs_filter,
-                            columns=['f1', 'f2', 'f3']
-                           ).to_pandas(), np.uint)
-    return np.concatenate([qs_in_f_arr, f1_in_qs_arr], axis=0)
-
-def filter_pq_simple(in_file, query_record, query_start, query_end):
-# def filter_pq(in_file, query_record, query_start, query_end):
-    ''' Filter parquet bed file into k-mer shadow casted intervals. '''
-    pq_ds = ds.dataset(in_file, format="parquet")
-    chr_filter = (ds.field('f0') == query_record)
-    simple_filter = (query_start <= (ds.field('f1'))) & ((ds.field('f1') < query_end))
-    joint_filter = chr_filter & simple_filter
-    return np.array(pq_ds.to_table(
-             filter=joint_filter,
-             columns=['f1', 'f2', 'f3']
-            ).to_pandas(), np.uint)
-
-
-# def filter_pq_(in_file, query_record, query_start, query_end):
 def filter_pq(in_file, query_record, query_start, query_end):
     ''' Filter parquet bed file into k-mer shadow casted intervals. '''
     pq_ds = ds.dataset(in_file, format="parquet")
@@ -59,6 +26,7 @@ def filter_pq(in_file, query_record, query_start, query_end):
              columns=['f1', 'f2', 'f3'],
              use_threads=True
             )
+             # columns=['f0', 'f1', 'f2', 'f3'],
     # return np.array(pq_ds.to_table(
              # filter=joint_filter,
              # columns=['f1', 'f2', 'f3'],
@@ -66,8 +34,27 @@ def filter_pq(in_file, query_record, query_start, query_end):
             # ).to_pandas(), np.uint)
 
 
-def save_to_file(arr, query_record, out_file):
-    np.savetxt(out_file, arr, delimiter="\t", fmt=query_record+'\t%1i\t%1i\t%1i')
+def save_to_file(pq_ds, query_record, out_file):
+    np.savetxt(out_file,
+               np.array(pq_ds.to_pandas(), np.uint),
+               delimiter="\t", fmt=query_record+'\t%1i\t%1i\t%1i')
+
+# def save_to_file(pq_ds, query_record, out_file):
+def save_to_file_ip(pq_ds, query_record, out_file):
+    ''' In-progress function to write to a bed file.
+
+    Issues:
+        - include_header=True  : includes the header
+        - include_header=False : does not include header, but add ^@ symbol to make file into a binary
+        - does not include the header column chromosme type
+    '''
+    # import IPython; IPython.embed()  # TODO
+    # note: require post-processing to remove headers and add
+    # write_options = csv.WriteOptions(include_header=False, delimiter='\t')
+    write_options = csv.WriteOptions(include_header=True,
+                                     quoting_style='none',
+                                     delimiter='\t')
+    csv.write_csv(pq_ds, out_file, write_options=write_options)
 
 
 ################################################################################
@@ -88,7 +75,7 @@ def main(args):
     query_record, start_end = genome_region.split(':')
     query_start, query_end = map(int, start_end.split('-'))
     res = filter_pq(in_file, query_record, query_start, query_end)
-    # save_to_file(res, query_record, out_file)
+    save_to_file(res, query_record, out_file)
 
 
 if __name__ == "__main__":
