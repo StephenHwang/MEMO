e619494beabb300c3c80a70775645f0b8aef97c0
StephenHwang <sjhwang@ucsc.edu>
2024-03-12 16:31:46 -0400

    Separate index and length directories.

--- bin/summarize_logs.sh
+++ bin/summarize_logs.sh
@@ -12,6 +12,7 @@ echo \
 "
 Options:
   -f [LOG_FILE]          slurm log file
+  -l [LENGTH_DIR]        MONI length directory
   -i [INDEX_DIR]         MONI index directory
   -o [OUTDIR]            output directory
 "
@@ -23,11 +24,14 @@ if [ "$#" -eq 0 ] || [ "$1" = "-h" ]; then
 fi
 
 # parse flags
-while getopts "f:i:o:" OPTION
+while getopts "f:l:i:o:" OPTION
 do
     case $OPTION in
         f )
             LOG_FILE=$OPTARG
+            ;;
+        l )
+            LENGTH_DIR=$OPTARG
             ;;
         i )
             INDEX_DIR=$OPTARG
@@ -43,7 +47,7 @@ done
 
 
 HEADER=$(head -n1 $LOG_FILE | awk '{print $NF}')
-LENGTHS_SIZE=$(ls -l --block-size=M $INDEX_DIR/$HEADER.lengths | cut -d' ' -f5)
+LENGTHS_SIZE=$(ls -l --block-size=M $LENGTH_DIR/$HEADER.lengths | cut -d' ' -f5)
 INDEX_SIZE=$(ls -l --block-size=M $INDEX_DIR/$HEADER* | grep "thrbv\|slp" | awk '{print $5}' | tr -d 'M' | paste -sd+ | bc)
 echo -n "| $HEADER | $LENGTHS_SIZE |" > $OUTDIR/$HEADER.txt
 cat $LOG_FILE | grep "^\s" | tail -n23 | grep "Percent\|Elapsed\|Maximum" | awk '{print $NF}' | tr '\n' '|' >> $OUTDIR/$HEADER.txt
